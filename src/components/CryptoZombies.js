969719a0be6ff1b432d96be9fe84335d3bf697eb
Nicolas Maltais <nic.maltais@gmail.com>
2021-11-14 16:30:32 -0500

    Add ability for new accounts to create a single random zombie to get started

--- src/components/CryptoZombies.js
+++ src/components/CryptoZombies.js
@@ -10,52 +10,72 @@ function CryptoZombies() {
 
   const [army, setArmy] = useState([]);
 
-  const getZombiesByOwner = useRef(() => {});
-  getZombiesByOwner.current = (owner) => {
-    return cryptoZombies.methods.getZombiesByOwner(owner).call()
+  function getZombiesByOwner(owner) {
+    return cryptoZombies.methods.getZombiesByOwner(owner).call();
   }
 
-  const updateArmy = useRef(() => {});
-  updateArmy.current = (zombies) => {
-    zombies.forEach(zombie => {
-      getZombieDetails(zombie).then(
-        zombieDetails => setArmy( prevArmy => [...prevArmy, zombieDetails])
-      );
+  const refreshArmy = useRef(() => {});
+  refreshArmy.current = (account) => {
+    setArmy([]);
+    getZombiesByOwner(account).then(zombies => {
+      console.log({zombies});
+      zombies.forEach(zombie => {
+        getZombieDetails(zombie).then(
+          (zombieDetails) => {
+            console.log(zombieDetails);
+            setArmy( prevArmy => [...prevArmy, zombieDetails])
+          }
+        );
+      });
     });
   }
 
   useEffect(() => {
-    getZombiesByOwner.current(account).then(updateArmy.current);
-    setArmy([]);
-  }, [account, updateArmy, getZombiesByOwner]);
+    refreshArmy.current(account);
+  }, [account, refreshArmy]);
 
 
   function getZombieDetails(id) {
-    return cryptoZombies.methods.zombies(id).call()
+    return cryptoZombies.methods.zombies(id).call();
+  }
+  
+  function createRandomZombie(name = 'NoName') {
+    // This is going to take a while, so update the UI to let the user know
+    // the transaction has been sent
+    return cryptoZombies.methods.createRandomZombie(name)
+    .send({ from: account })
+    .on("receipt", function(receipt) {
+      console.log({receipt});
+      console.log("Successfully created " + name + "!")
+      // Transaction was accepted into the blockchain, let's redraw the UI
+      refreshArmy.current(account);
+    })
+    .on("error", function(error) {
+      console.error({error});
+      // Do something to alert the user their transaction has failed
+    });
   }
   // function zombieToOwner(id) {
   //   return cryptoZombies.methods.zombieToOwner(id).call()
   // }
 
-  // let armyPics = army.map(zombie => <li><Zombie2 key={zombie.dna} dna={zombie.dna} /></li>);\
+  const renderedArmy = army.map(zombie => <li key={zombie.dna}><Zombie dna={zombie.dna}/></li>);
 
   return (
     <div>
       {army.length ?
         <div>
           <h1>Your army:</h1>
-          <ul>{army.map(zombie => <li key={zombie.dna}><Zombie dna={zombie.dna} /></li>)}</ul>
+          <ul>{renderedArmy}</ul>
         </div>
       :
-      `You have no zombies`}
+        <div>
+          <h1>You have no zombies</h1>
+          <button onClick={() => createRandomZombie()}>Give me a random Zombie</button>
+        </div>
+      }
     </div>
   )
-  // return (
-  //   army.length ?
-  //   `Your army of zombies: ${JSON.stringify(army)}`
-  //   :
-  //   `You have no zombies`
-  // )
 }
 
 export default CryptoZombies;
